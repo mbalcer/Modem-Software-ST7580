6cc787b7917b3c4e3e18272532061c6a9152b71c
mbalcer <mateusz75319@gmail.com>
2019-11-05 17:59:21 +0100

    Refactored code with use PortCOM class

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,17 +1,13 @@
 package controller;
 
 import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.paint.Paint;
-import model.Frame;
+import model.PortCOM;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 public class MainController {
@@ -46,8 +42,7 @@ public class MainController {
     @FXML
     private Label info;
 
-    private SerialPort connectedPort;
-    private Frame frame;
+    private PortCOM connectedPort;
 
     public void initialize() {
         fillComboBox();
@@ -64,102 +59,48 @@ public class MainController {
 
     @FXML
     public void connect() {
-        connectedPort.openPort();
-        connectedPort.setBaudRate(57600);
-        frame = new Frame();
-        connectedPort.addDataListener(new SerialPortDataListener() {
-            @Override
-            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
-            @Override
-            public void serialEvent(SerialPortEvent event)
-            {
-                byte[] getByte = new byte[1];
-                InputStream in = connectedPort.getInputStream();
-
-
-                while(connectedPort.bytesAvailable() > 0) {
-                    try {
-                        in.read(getByte, 0, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    frame.processFrame(getByte[0]);
-                }
-
-                if (frame.isCorrectFrame()) {
-                    Arrays.stream(frame.getData())
-                            .forEach(data -> receivedText.setText(receivedText.getText() + String.format("%02x", data) + " "));
-
-                    receivedText.setText(receivedText.getText() + "\n");
-                }
-            }
-        });
-
+        connectedPort = new PortCOM(getPortFromCheckbox(), receivedText);
+        connectedPort.open();
         checkOpenPort(connectedPort);
     }
 
     @FXML
     public void reset() {
         final byte[] resetCode = {0x02, 0x00, 0x3C, 0x3C, 0x00};
-        sendText(resetCode);
+        connectedPort.send(resetCode);
     }
 
     @FXML
     public void send() {
         String message = textToSend.getText();
-        sendText(message.getBytes());
-    }
-
-    public void sendText(byte[] bytes) {
-        connectedPort.setDTR();
-        connectedPort.setRTS();
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        connectedPort.writeBytes(bytes, bytes.length);
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        connectedPort.clearDTR();
-        connectedPort.clearRTS();
+        connectedPort.send(message.getBytes());
     }
 
     @FXML
     public void disconnect() {
-        connectedPort.removeDataListener();
-        connectedPort.closePort();
+        connectedPort.close();
         checkClosePort(connectedPort);
     }
 
-    @FXML
-    public void changeConnectedPort() {
-        connectedPort = getPortFromCheckbox();
-    }
-
-    private void checkOpenPort(SerialPort port) {
-        if (port.isOpen()) {
-            info.setText("You are connected to the port " +port.getSystemPortName());
+    private void checkOpenPort(PortCOM port) {
+        if (port.getPort().isOpen()) {
+            info.setText("You are connected to the port " +port.getPort().getSystemPortName());
             info.setTextFill(Paint.valueOf("GREEN"));
             disableButtons(false);
         }
         else {
-            info.setText("Unable to connect to port " + port.getSystemPortName());
+            info.setText("Unable to connect to port " + port.getPort().getSystemPortName());
             info.setTextFill(Paint.valueOf("RED"));
         }
     }
 
-    private void checkClosePort(SerialPort port) {
-        if (port.isOpen()) {
-            info.setText("Cannot disconnect from port " +port.getSystemPortName());
+    private void checkClosePort(PortCOM port) {
+        if (port.getPort().isOpen()) {
+            info.setText("Cannot disconnect from port " +port.getPort().getSystemPortName());
             info.setTextFill(Paint.valueOf("RED"));
         }
         else {
-            info.setText("Disconnected from the port " + port.getSystemPortName());
+            info.setText("Disconnected from the port " + port.getPort().getSystemPortName());
             info.setTextFill(Paint.valueOf("GREEN"));
             disableButtons(true);
         }
